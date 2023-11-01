3cecd07b6e88da514ebbb0baa6b9821772938558
Liangdi <wu@liangdi.me>
2023-11-02 02:18:06 +0800

    添加数据集 解压工具类

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use candle_mnist_demo::utils::gz_decoder;
+use tracing::info;
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt::init();
+    info!("Hello, world!");
+
+    let dataset_dir = "datasets/mnist";
+
+    gz_decoder::decompress_dataset(dataset_dir);
 }
